0b330709e76513f80c974251883edc03ad4bdf49
shortboy7 <thdwldnd7@dgu.ac.kr>
2022-06-23 18:27:46 +0900

    add filtered contents

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, ipcMain, webContents} = require("electron");
+const {app, Menu,BrowserWindow, ipcMain, webContents} = require("electron");
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 const Iconv = require('iconv').Iconv;
@@ -32,6 +32,39 @@ let site = [
 ];
 
 let contents = [];
+let keywords = [];
+let filteredContents = [];
+let main;
+
+function leftPad(value){
+    if(value >= 10) return value;
+    else return `0${value}`;
+}
+
+function toStringByFormat(source , delimiter = '-'){
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    console.log(month);
+    const day = leftPad(source.getDate());
+
+    return [year, month, day].join(delimiter);
+}
+
+function filterContents(){
+    let newFilteredContents = [];
+    const dateString = toStringByFormat(new Date(), '.');
+    for(const content of contents){
+        // if(content.date < dateString) continue;
+        for(const keyword of keywords){
+            if(content.title.includes(keyword)){
+                newFilteredContents.push(content);
+                break;
+            }
+        }
+    }
+    filteredContents = newFilteredContents;
+}
+
 const getView = (url) => {
     const newWindow = new BrowserWindow({
         webPreferences : {
@@ -59,16 +92,6 @@ const getUrl = {
             // responseEncoding:'utf8' //UTF-8 , binary, 
         });
         const $ = cheerio.load(response.data.toString());
-        for(let i = 0; i < 6 ; i++){
-            const date = $('.info span:first-child')[i].children[0].data;
-            const title = $('.tit')[i].children[2].data.trim(' ');
-            const detailId = $('.board_list ul li a')[i].attribs['onclick'].substring(9,17);
-            contents.push({
-                title:title, 
-                date:date, 
-                detailUrl : `${urlbase}${board}/detail/${detailId}`
-            });
-        }
         for(let i = 6; i < $('.tit').length; i++){
             const title = $('.tit')[i].children[0].data.trim(' ');
             const date = $('.info span:first-child')[i].children[0].data;
@@ -80,23 +103,43 @@ const getUrl = {
                 detailUrl : `${urlbase}${board}/detail/${detailId}`
             });
         }
-        Promise.resolve(contents);
+        filterContents();
+        main.webContents.send('init', filteredContents);
+        Promise.resolve();
     }
 };
 
 async function createWindow(){
-    const win = new BrowserWindow({
+    main.loadFile("./src/index.html");
+    initContents();
+}
+
+async function initContents() {
+    for(let s = 0 ; s <= 0 ; s++){
+        for(let b = 0 ; b < site[0].board.length; b++){
+            for(let p = 1; p < 3; p++){
+                setTimeout(() => {
+                    getUrl['dongguk'](site[s].url, p, site[s].board[b]);
+                }, 1000);
+            }
+        }
+    }
+}
+
+app.on('ready', () => {
+    main = new BrowserWindow({
         webPreferences : {
             nodeIntegration: true,
             contextIsolation: false,
             enableRemoteModule: false    
         }
     });
-    win.loadFile("./src/index.html");
-    await getUrl['dongguk'](site[0].url, 3, site[0].board[0])
-    win.webContents.send('init', contents);
-}
+    let str;
+    fs.readFile('./keywords.json',{encoding:'utf-8',flag:'r+'},(err, data) =>{
+        console.log(data);
+        const obj = JSON.parse(data);
+        keywords = obj['keywords'];
+    });
 
-app.on('ready', () => {
     createWindow();
 });
